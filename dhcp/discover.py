8270ca73c4ea2cffb3ad7d4a9e55dd710db551cb
MDThomsen <mdahlthomsen@hotmail.com>
2019-02-27 16:46:43 +0100

    added mud_url to discover spoof

--- dhcp/discover.py
+++ dhcp/discover.py
@@ -1,7 +1,7 @@
 class DHCPSpoofing:
 
     @staticmethod
-    def send_dhcp_discover(mac: str, dhcp_fingerprint: list, dhcp_vendor: str):
+    def send_dhcp_discover(mud_url: str, mac: str, dhcp_fingerprint: list, dhcp_vendor: str):
         """
         To be used for testing only. Sends a spoofed DHCP Discover packet to the default ethernet interface of the localhost.
         :param mac: mac address of the spoofed device.
@@ -26,7 +26,7 @@ class DHCPSpoofing:
                         /IP(src='0.0.0.0', dst='255.255.255.255')\
                         /UDP(dport=67, sport=68)\
                         /BOOTP(chaddr=localmacraw,xid=RandInt())\
-                        /DHCP(options=[('message-type', 'discover'),('param_req_list',)+tuple([chr(x) for x in dhcp_fingerprint]),('vendor_class_id',dhcp_vendor), 'end'])
+                        /DHCP(options=[('message-type', 'discover'),('param_req_list',)+tuple([chr(x) for x in dhcp_fingerprint]),('vendor_class_id',dhcp_vendor), (161,mud_url), 'end'])
 
         #https://programtalk.com/python-examples/scapy.all.DHCPRevOptions/
 
